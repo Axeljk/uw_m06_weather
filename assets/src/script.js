d70a489a2defab099ee16ed9f7b7207138fce138
axeljk <axeljkern@yahoo.com>
2022-09-07 11:39:18 -0400

    Adds fetch requests for UV index and five-day forecast in loop.

--- assets/src/script.js
+++ assets/src/script.js
@@ -14,6 +14,7 @@ window.onload = () => {
 			addCityHistory(cityHistory[i])
 	}
 
+	// Submit button for text field.
 	$("#cityButton").on("click", (event) => {
 		let cityName = "";
 
@@ -24,7 +25,6 @@ window.onload = () => {
 		fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=e9c23417e6f0f3c2caee34799ba0b8e7")
 			.then((response) => response.json())
 			.then((responseJson) => {
-				console.log( responseJson[0].name);
 				if (responseJson.length > 0 && responseJson[0].name.toUpperCase() === cityName.toUpperCase()) {
 					// City exists. Create object for it:
 					currentCity = {
@@ -32,6 +32,7 @@ window.onload = () => {
 						lat: responseJson[0].lat,
 						lon: responseJson[0].lon
 					}
+					console.log("Current", currentCity);
 
 					// Add city to the recent list.
 					cityHistory.push(currentCity);
@@ -41,6 +42,16 @@ window.onload = () => {
 					weatherPopulate();
 				}
 			})
+	});
+
+	// Previous cities buttons.
+	$("ul button").on("click", (event) => {
+		currentCity = {
+			name:  $(event.target).text(),
+			lat: $(event.target).data("lat"),
+			lon: $(event.target).data("lon")
+		}
+		weatherPopulate();
 	});
 }
 
@@ -67,5 +78,43 @@ function addCityHistory(city) {
 }
 
 function weatherPopulate() {
+	// Current Weather
+	fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + currentCity.lat + "&lon=" + currentCity.lon + "&cnt=5&appid=e9c23417e6f0f3c2caee34799ba0b8e7")
+		.then((response) => response.json())
+		.then((responseJson) => {
+			console.log(responseJson)
+			$("#curCity").text(responseJson.name);
+			$("#curDate").text(moment(responseJson.dt, "X").format("MM/DD/YYYY"));
+			$("#curIcon").attr("src", "http://openweathermap.org/img/wn/" + responseJson.weather[0].icon + ".png");
+			$("#curTemp").text("Temp: " + ((responseJson.main.temp - 273.15) * 9 / 5 + 32).toFixed(0) + "°F");
+			$("#curWind").text("Wind: " + responseJson.wind.speed + "MPH");
+			$("#curHumidity").text("Humidity: " + responseJson.main.humidity + "%");
+		});
 
+	// UV Index
+	fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + currentCity.lat + "&lon=" + currentCity.lon + "&cnt=5&appid=e9c23417e6f0f3c2caee34799ba0b8e7")
+	.then((response) => response.json())
+	.then((responseJson) => {
+		$("#curUV").text(responseJson.current.uvi);
+		if (responseJson.current.uvi <= 2) // Favorable
+			$("#curUV").css("background-color", "#090");
+		else if (responseJson.current.uvi <= 5) // Moderate
+			$("#curUV").css("background-color", "#aa0");
+		else
+			$("#curUV").css("background-color", "#900");
+	});
+
+	// Forecast
+	fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity.name + "&appid=e9c23417e6f0f3c2caee34799ba0b8e7")
+		.then((response) => response.json())
+		.then((responseJson) => {
+			for (let i = 1; i <= 5; i++) {
+				let forecastIndex = responseJson.list[8 * i - 1];
+				$(`#date${i}`).text(moment(forecastIndex.dt, "X").format("MM/DD/YYYY hh:mm a"));
+				$(`#date${i}Icon`).attr("src", "http://openweathermap.org/img/wn/" + forecastIndex.weather[0].icon + ".png");
+				$(`#date${i}Temp`).text(((forecastIndex.main.temp - 273.15) * 9 / 5 + 32).toFixed(0) + "°F");
+				$(`#date${i}Wind`).text(forecastIndex.wind.speed + "MPH");
+				$(`#date${i}Humidity`).text(forecastIndex.main.humidity + "%");
+			}
+		});
 }
